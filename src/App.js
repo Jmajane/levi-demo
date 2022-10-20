2808f1fa1399df2df9fef9e465bf48ee93551786
Jmajane <jordan.majane@outlook.com>
2022-10-20 13:25:33 -0400

    star rating added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import './App.css';
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
 
+import { styled } from '@mui/material/styles'
+import Rating from '@mui/material/Rating';
 
 function App() {
+
+  const StyledRating = styled(Rating)({
+    '& .MuiRating-iconFilled': {
+      color: 'black'
+    }
+  })
+
   return (
     <div className="App">
       <Nav /> 
       <Header />
+
+      <div>
+
+
+        <div className='Jeans'>
+          <h3>501 ORIGINAL FIT MEN'S JEANS</h3>
+          <StyledRating name="four-star" defaultValue={4.3} precision={0.1} />
+        </div>
+      </div>
     </div>
   );
 }
