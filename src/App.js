a8bc0ce28fc5a6152c4182e4d82f7877e9b07189
Jmajane <jordan.majane@outlook.com>
2022-10-20 13:33:43 -0400

    Top right section of jeans information added

--- src/App.js
+++ src/App.js
@@ -24,6 +24,10 @@ function App() {
         <div className='Jeans'>
           <h3>501 ORIGINAL FIT MEN'S JEANS</h3>
           <StyledRating name="four-star" defaultValue={4.3} precision={0.1} />
+          <p className='Grey-ouy'>(3111)</p>
+          <h2 className='Price'>$79.50</h2>
+          <p className='Grey-out'>Or 4 installments of $19.88</p>
+          <p className='Deal'>2 for $99. Applied at Checkout</p>
         </div>
       </div>
     </div>
