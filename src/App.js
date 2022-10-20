d7504b412d324d8372057490f9aa5455a624753f
Jmajane <jordan.majane@outlook.com>
2022-10-20 13:09:56 -0400

    header information added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
 
 
 function App() {
   return (
     <div className="App">
-      <Nav />
+      <Nav /> 
+      <Header />
     </div>
   );
 }
