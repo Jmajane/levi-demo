b3d518fde2df6d28c598ed603b3bbd784d642f76
Jmajane <jordan.majane@outlook.com>
2022-10-20 12:31:30 -0400

    initial setup of files and commonents

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./nav.css"
+
+import {AiOutlineMenu} from 'react-icons/ai'
+import {AiOutlineSearch} from 'react-icons/ai'
+import {AiOutlineShopping} from 'react-icons/ai'
+
+
+
+function Nav() {
+  return (
+    <div className='nav'>
+        <AiOutlineMenu />
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Levi%27s_logo.svg/2560px-Levi%27s_logo.svg.png' alt='Levi'></img>
+
+        <AiOutlineSearch />
+        <AiOutlineShopping />
+    </div>
+  )
+}
+
+export default Nav
