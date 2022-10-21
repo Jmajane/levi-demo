77a57dcc2c82e2435d537041500b6b20215ada44
Charles Bartlett <ownageunit@gmail.com>
2022-10-21 12:45:45 -0400

    WIP filenames changed

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="header">
+      <h5>SHOP</h5>
+      <h5>DISCOVER</h5>
+      <h5>SECONDHAND</h5>
+
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Levi%27s_logo.svg/2560px-Levi%27s_logo.svg.png"
+        alt="Levi"
+      ></img>
+    </div>
+  );
+}
+
+export default Nav;
