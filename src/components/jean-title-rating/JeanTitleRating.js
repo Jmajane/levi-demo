57d6622ad7e5231a29172399f0a57d31546022e1
Jmajane <jordan.majane@outlook.com>
2022-10-23 14:22:55 -0400

    Jean title and rating moved to seperate component

---
+++ src/components/jean-title-rating/JeanTitleRating.js
@@ -0,0 +1,70 @@
+const JeanTitleRating = [
+    {
+       style: "501 ORIGINAL FIT MEN'S JEANS",
+       rating: 4.3, 
+    },
+    {
+       style: "501 ORIGINAL FIT MEN'S JEANS",
+       rating: 4.3, 
+    },
+    {
+       style: "501 ORIGINAL FIT MEN'S JEANS",
+       rating: 4.1, 
+    },
+    {
+       style: "501 ORIGINAL FIT MEN'S JEANS",
+       rating: 4.2, 
+    },
+    {
+       style: "501 ORIGINAL FIT MEN'S JEANS",
+       rating: 4.1, 
+    },
+    {
+       style: "501 ORIGINAL FIT MEN'S JEANS",
+       rating: 4.3, 
+    },
+    {
+       style: "501 ORIGINAL FIT MEN'S JEANS",
+       rating: 4.2, 
+    },
+    {
+       style: "501 ORIGINAL FIT MEN'S JEANS",
+       rating: 4.1, 
+    },
+    {
+       style: "501 ORIGINAL FIT MEN'S JEANS",
+       rating: 4.1, 
+    },
+    {
+       style: "501 ORIGINAL FIT MEN'S JEANS",
+       rating: 4.1, 
+    },
+    {
+       style: "501 ORIGINAL FIT MEN'S JEANS",
+       rating: 4.6, 
+    },
+    {
+       style: "501 ORIGINAL FIT MEN'S JEANS",
+       rating: 4.6, 
+    },
+    {
+       style: "501 ORIGINAL FIT MEN'S JEANS",
+       rating: 4.1, 
+    },
+    {
+       style: "501 ORIGINAL FIT MEN'S JEANS",
+       rating: 4.3, 
+    },
+    {
+       style: "501 ORIGINAL FIT MEN'S JEANS",
+       rating: 4.3, 
+    },
+    {
+       style: "501 ORIGINAL FIT MEN'S JEANS",
+       rating: 4.3, 
+    },
+    {
+       style: "501 ORIGINAL FIT MEN'S JEANS",
+       rating: 0.0, 
+    },
+]
