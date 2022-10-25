9399b2ef2d3c31eac0861d4e524b98a54fa3756c
Jmajane <jordan.majane@outlook.com>
2022-10-25 12:10:29 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { useState } from "react";
 import MobileNav from "./components/mobile-nav/MobileNav";
 import Nav from "./components/nav/Nav";
 import MediaGallery from "./components/MediaGallery/MediaGallery";
+import JeanTitleRating from "./components/JeanTitleRating/JeanTitleRating";
+import JeanPrice from "./components/jean-price/JeanPrice";
+
 
 import jeanData from "./jeanData";
 import StyleSelector from "./components/StyleSelector/StyleSelector";
@@ -16,11 +19,10 @@ function App() {
     <div className="App">
       <Nav />
       <MobileNav />
-      {/* <JeanTitleRating jeanColor={selectedJean} />
-      <JeanPrice jeanColor={selectedJean} />
-      <JeanSelector jeanColor={selectedJean} colors={jeanData} /> */}
+      <JeanTitleRating jeanColor={selectedStyle} />
       <MediaGallery jeanData={jeanData} selectedStyle={selectedStyle} />
       <StyleSelector styles={jeanData.styles} />
+      <JeanPrice jeanColor={selectedStyle} /> 
     </div>
   );
 }
--- src/components/jean-price/JeanPrice.js
+++ src/components/jean-price/JeanPrice.js
@@ -4,7 +4,7 @@ import './JeanPrice.css'
 function JeanPrice({ jeanColor }) {
   return (
     <div>
-    <h2>{jeanColor.styles[0].price}</h2>    
+    <h2>{jeanColor.price}</h2>    
     <p className="Grey-out">Or 4 installments of $27.00</p>
     </div>
   )
