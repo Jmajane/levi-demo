b29b89b674dad195920e6f3887c9fa554c309fd5
Charles Bartlett <ownageunit@gmail.com>
2022-10-25 03:46:24 -0400

    mobile view done

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import StyleSelector from "./components/StyleSelector/StyleSelector";
 import SizeSelector from "./components/SizeSelector/SizeSelector";
 
 function App() {
-  const [selectedStyle, setSelectedStyle] = useState(jeanData.styles[1]);
+  const [selectedStyle, setSelectedStyle] = useState(jeanData.styles[0]);
   const [selectedPicture, setSelectedPicture] = useState(
     selectedStyle.pictures[0]
   );
@@ -20,20 +20,24 @@ function App() {
     <div className="App">
       <Nav />
       <MobileNav />
-
-      <MediaGallery
-        jeanData={jeanData}
-        selectedStyle={selectedStyle}
-        selectedPicture={selectedPicture}
-        setSelectedPicture={setSelectedPicture}
-      />
-      <StyleSelector
-        styles={jeanData.styles}
-        selectedStyle={selectedStyle}
-        setSelectedStyle={setSelectedStyle}
-        setSelectedPicture={setSelectedPicture}
-      />
-      <SizeSelector selectedStyle={selectedStyle} />
+      <div className="MobileView">
+        <MediaGallery
+          jeanData={jeanData}
+          selectedStyle={selectedStyle}
+          selectedPicture={selectedPicture}
+          setSelectedPicture={setSelectedPicture}
+        />
+        <div className="Divider">
+          <StyleSelector
+            styles={jeanData.styles}
+            selectedStyle={selectedStyle}
+            setSelectedStyle={setSelectedStyle}
+            setSelectedPicture={setSelectedPicture}
+          />
+          <SizeSelector selectedStyle={selectedStyle} />
+        </div>
+      </div>
+      <div className="DesktopView"></div>
     </div>
   );
 }
