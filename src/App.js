27c2becb016982cee06e3e0e0b70a523c1fb896f
Charles Bartlett <ownageunit@gmail.com>
2022-10-24 21:42:58 -0400

    reorganized seed data to optimize components

--- src/App.js
+++ src/App.js
@@ -5,22 +5,23 @@ import { useState } from "react";
 import MobileNav from "./components/mobile-nav/MobileNav";
 import Nav from "./components/nav/Nav";
 import JeanSelector from "./components/style-selector/StyleSelector";
-import jeanData from "./components/jean-data/JeanData";
 import JeanTitleRating from "./components/jean-title-rating/JeanTitleRating";
 import JeanPrice from "./components/jean-price/JeanPrice";
+import MediaGallery from "./components/media-gallery/MediaGallery";
 
+import jeanData from "./jeanData";
 
 function App() {
-  const [selectedJean, setSelectedJean] = useState(jeanData[0]);
+  const [selectedStyle, setSelectedStyle] = useState(jeanData);
 
   return (
     <div className="App">
       <Nav />
       <MobileNav />
-      <JeanTitleRating jeanColor={selectedJean} />
+      {/* <JeanTitleRating jeanColor={selectedJean} />
       <JeanPrice jeanColor={selectedJean} />
-      <JeanSelector jeanColor={selectedJean} colors={jeanData} />
-
+      <JeanSelector jeanColor={selectedJean} colors={jeanData} /> */}
+      <MediaGallery selectedStyle={selectedStyle} />
     </div>
   );
 }
--- src/components/jean-title-rating/JeanTitleRating.js
+++ src/components/jean-title-rating/JeanTitleRating.js
@@ -1,27 +1,22 @@
-import "./JeanTitleRating.css"
+import "./JeanTitleRating.css";
 
 import { styled } from "@mui/material/styles";
 import Rating from "@mui/material/Rating";
 
-function JeanTitleRating({ jeanColor }) {
-    
-    const StyledRating = styled(Rating)({
-        "& .MuiRating-iconFilled": {
-          color: "black",
-        },
-      });
+function JeanTitleRating({ title, rating, votes }) {
+  const StyledRating = styled(Rating)({
+    "& .MuiRating-iconFilled": {
+      color: "black",
+    },
+  });
 
   return (
     <div>
-        <h3>{jeanColor.title}</h3>
-        <StyledRating
-        name="star-rating"
-        defaultValue={jeanColor.styles[0].rating}
-        precision={0.1} 
-      />
-      <p>{jeanColor.styles[0].votes}</p>
+      <h3>{title}</h3>
+      <StyledRating name="star-rating" defaultValue={rating} precision={0.1} />
+      <p>{votes}</p>
     </div>
-  )
+  );
 }
 
-export default JeanTitleRating
+export default JeanTitleRating;
