e43a3967a370d805f786defa66235d4084af55dd
Charles Bartlett <ownageunit@gmail.com>
2022-10-27 14:08:22 -0400

    basic login page for popup widget functional no css

---
+++ src/components/TylmenWidget/PopupWidget/Login/Login.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const LoginState = () => {
+  const handleChange = (e) => {
+    setFormState((prevState) => {
+      return { ...prevState, [e.target.name]: e.target.value };
+    });
+  };
+
+  const [formState, setFormState] = useState({ email: "", password: "" });
+
+  return (
+    <div id="LoginState">
+      <h1>Tylmen</h1>
+      <form>
+        <label>Email</label>
+        <input
+          type="text"
+          name="email"
+          value={formState.email}
+          onChange={(e) => handleChange(e)}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          value={formState.password}
+          onChange={(e) => handleChange(e)}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default LoginState;
